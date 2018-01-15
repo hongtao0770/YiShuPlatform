70c43cc794fcee32ad47bff7d4377a25a9499f53
ht783608708 <33300443+ht783608708@users.noreply.github.com>
2018-01-15 19:26:05 +0800

    Update ezModule.js

--- YiShuBookPlatform/YiShu/www/js/controller/ezModule.js
+++ YiShuBookPlatform/YiShu/www/js/controller/ezModule.js
@@ -1,25 +1,4 @@
 angular.module('ezModule',[]).controller('ezCtrl',function($scope,$state){
    $state.go("login");
-    // $state.go("register");
-  //$state.go("list.homePage");
-    // $state.go("list.bookStore");
-    //$state.go('bookInfo');
-   //$state.go('bookInfo2');
-   //$state.go('list.friendsCircle');
-    //$state.go('list.activity');
-    //$state.go('ensureOrder');
-    //$state.go('ensureOrder2');
-    //$state.go('order');
-     //$state.go('personalCenter');
-    //$state.go('myFriend');
-    //$state.go('aboutUs');
-    //$state.go('forgetInfo');
-    //$state.go('uploadhead');
-     //$state.go('myNotice');
-    // $state.go('uploadBook');
-   // $state.go('linkAdmin');
-   //$state.go('activityInfo');
-   //  $state.go('uploadRecord');
-   //  192.168.43.253
     apiRoot = 'http://localhost:8080/YiShuBookPlatform/service/Api/index.php/home/';
 })
